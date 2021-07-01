c3645d40a580aed7f9d4877731623285d85ba350
Tom McCauley <tpmccauley@gmail.com>
2021-07-01 13:26:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# CMS Masterclass
+
+A prototype masterclass for CMS using interactive 3D event displays
+
+
+To run (this application requires python3):
+
+```
+git clone https://github.com/QuarkNet-HEP/cms-masterclass.git
+cd cms-masterclass
+pip install -r requirements.txt
+export FLASK_DEBUG=1
+flask run
+```
+
+
+Then go to `http://127.0.0.1:5000`
---
+++ app.py
@@ -0,0 +1,58 @@
+import json
+
+from flask import (
+    Flask,
+    render_template
+)
+
+from flask_bootstrap import (
+    Bootstrap
+)
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+zh_events = json.load(
+    app.open_resource('data/test.json')
+)
+
+print(
+    zh_events
+)
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        title='CMS Masterclass',
+    )
+
+@app.route('/contents')
+def contents():
+    return render_template(
+        'contents.html',
+        title='Contents',
+    )
+
+@app.route('/events')
+def events():
+    return render_template(
+        'events.html',
+        title='Events',
+        events=zh_events,
+    )
+
+@app.route('/events/<id>')
+def event(id):
+    return render_template(
+        'event.html',
+        title=f'Event {id}',
+        nevents=len(zh_events),
+        back_id=int(id)-1,
+        next_id=int(id)+1,
+        event=list(filter(lambda e: e["id"] == id, zh_events))[0],
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
